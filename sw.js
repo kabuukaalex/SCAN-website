58c2c62a4a5d094d8a140dc959b9e4ab6d8b7dd2
kabuukaalex <kabuukaalex4@gmail.com>
2026-08-30 11:00:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
-
+const CACHE = "scan-v2";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/assets/scan-logo.png",
+  "/assets/kabuuka-alex.png"
+];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      return caches.open(CACHE).then(cache => {
+        cache.put(e.request, res.clone());
+        return res;
+      });
+    })).catch(()=>caches.match("/index.html"))
+  );
+});
